1fed960da0f016b2d6fc3a6536a6f7587ae46937
YASH PATEL <44220455+yash982000@users.noreply.github.com>
2020-06-01 15:50:24 +0530

    Add files via upload

---
+++ 1097B.cpp
@@ -0,0 +1,23 @@
+// YASH PATEL
+
+#include<iostream>
+#include<algorithm>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+	int T, a[T];
+	cin >> T;
+	while(T--){
+		for(int i=1;i<=T;i++)
+		{
+			scanf("%d",&a[i]);
+		}
+	}
+	
+}
