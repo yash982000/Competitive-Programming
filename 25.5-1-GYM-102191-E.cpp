3b74541f9f105cab7196508c3c8e287473cd2b66
YASH PATEL <44220455+yash982000@users.noreply.github.com>
2020-06-01 15:48:09 +0530

    Add files via upload

---
+++ 25.5-1-GYM-102191-E.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	string str;
+	//char s[100];
+    //n=strlen(s);
+    n=str.size();
+	for(int i=1;i<=str.size();i++){
+		cin >> n;
+	}
+}
